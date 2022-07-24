3c91cc6c1dae49c445e857b004ec99363c8c3a85
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-07-24 22:43:12 +0100

    Bootstrapping project

---
+++ src/main/java/io/codewithgx/springchallenges/SpringChallengesApplication.java
@@ -0,0 +1,13 @@
+package io.codewithgx.springchallenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringchallengesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringchallengesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/codewithgx/springchallenges/SpringChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codewithgx.springchallenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringchallengesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
