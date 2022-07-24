6a7a21ac1e3a17bd7a7d51e4fb1b3cea76319d22
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-07-24 22:43:46 +0100

    Renaming and testing contextLoads

--- src/main/java/io/codewithgx/springchallenges/SpringChallengesApplication.java
+++ src/main/java/io/codewithgx/springchallenges/SpringChallengesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringchallengesApplication {
+public class SpringChallengesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringchallengesApplication.class, args);
+		SpringApplication.run(SpringChallengesApplication.class, args);
 	}
 
 }
--- src/test/java/io/codewithgx/springchallenges/SpringChallengesApplicationTests.java
+++ src/test/java/io/codewithgx/springchallenges/SpringChallengesApplicationTests.java
@@ -1,13 +1,21 @@
 package io.codewithgx.springchallenges;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringchallengesApplicationTests {
+class SpringChallengesApplicationTests {
+
+	@Autowired
+	protected SpringChallengesApplication application;
 
 	@Test
 	void contextLoads() {
+		assertThat(application).isNotNull();
 	}
 
 }
